358834122afa080ae0621870fbfc3e7f8ab8e925
liuzhe <zhethink@gmail.com>
2018-11-13 11:02:55 +0800

    改回parcel index.html

--- dist/index.js
+++ dist/index.js
@@ -198,7 +198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61324" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62284" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
